6950e41021c0ae143951e4bdc22263739791efbe
daizhi <dz_work_contact@163.com>
2018-07-29 11:37:16 +0800

    eslint optimization

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  plugins: {
-    //自动补充各浏览器前缀
-    'autoprefixer': {browsers: 'last 5 version'}
-  }
+    plugins: {
+        // 自动补充各浏览器前缀
+        'autoprefixer': {browsers: 'last 5 version'}
+    }
 }
